8f27f67898c018740442b4e1085d45152fb90706
Brian P. Holt <bholt@dwolla.com>
2015-12-25 11:29:56 -0600

    task definitions should map on upstream tasks and settings

--- src/main/scala/com/dwolla/sbt/docker/DockerContainerPlugin.scala
+++ src/main/scala/com/dwolla/sbt/docker/DockerContainerPlugin.scala
@@ -15,40 +15,37 @@ object DockerContainerPlugin extends AutoPlugin {
 
   override def requires = DockerPlugin
 
-  lazy val baseDockerContainerSettings = Seq(
+  lazy val defaultValues = Seq(
     dockerContainerName := name.value,
     (version in createLocal) := version.value,
-    dockerContainerMemoryLimit := None,
+    dockerContainerMemoryLimit := None
+  )
 
-    createLocal := {
-      val imageName = (dockerTarget in Docker).value
-      (publishLocal in Docker).value
+  lazy val tasks = Seq(
+    createLocal <<= (dockerContainerMemoryLimit, dockerTarget in Docker, dockerContainerName, publishLocal) map { (optionalMemoryLimit, imageName, containerName, _) ⇒
+      val memoryLimit = optionalMemoryLimit.map(x ⇒ s"-m $x").getOrElse("")
 
-      val memoryLimit = dockerContainerMemoryLimit.value.map(x ⇒ s"-m $x").getOrElse("")
+      s"docker create --name $containerName $memoryLimit $imageName" !
 
-      s"docker create --name ${dockerContainerName.value} $memoryLimit $imageName" !
-
-      dockerContainerName.value
+      containerName
     },
-    runLocal := {
-      s"docker start ${createLocal.value}" !
+    runLocal <<= createLocal map { containerName ⇒
+      s"docker start $containerName" !
     },
-    clean := {
-      clean.value
-      val containerName = dockerContainerName.value
-      val imageName = (dockerTarget in Docker).value
+    clean <<= (dockerContainerName, dockerTarget in Docker, clean) map { (containerName, imageName, _) ⇒
       s"docker stop $containerName" !;
       s"docker rm $containerName" !;
       s"docker rmi $imageName" !
     })
+
+  lazy val baseDockerContainerSettings = defaultValues ++ tasks
 
   override lazy val projectSettings = baseDockerContainerSettings
 }
 
 trait DockerContainerKeys {
   lazy val createLocal = TaskKey[String]("createLocal", "Use the newly-built image and create a local container with the right parameters")
-  lazy val runLocal = TaskKey[Unit]("runLocal", "build and publish the docker container, then start it")
-  lazy val cleanDocker = TaskKey[Unit]("clean", "stop the service, remove its container and image")
+  lazy val runLocal = TaskKey[Unit]("runLocal", "Build and publish the docker container, then start it")
 
   lazy val dockerContainerName = settingKey[String]("Name of the container to be created. Defaults to project name")
   lazy val dockerContainerMemoryLimit = settingKey[Option[String]]("memory limit for created Docker container. e.g., Option('192M')")
